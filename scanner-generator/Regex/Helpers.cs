79ef5bd2063cc8f03585a5193edfd6107cf9d7cc
Roberto Solares <betosolaresgar@gmail.com>
2020-03-02 17:33:07 -0600

    Create method to check the precedence of a certain token

--- scanner-generator/Regex/Helpers.cs
+++ scanner-generator/Regex/Helpers.cs
@@ -6,6 +6,18 @@ namespace scanner_generator.Regex
 {
     class Helpers
     {
+        /// <summary>Check the precedence of the token</summary>
+        /// <param name="token">The token to check the preference</param>
+        /// <param name="lastOperator">The operator to compare</param>
+        /// <param name="operators">The list of operators</param>
+        /// <returns>True if the token precedence is less or equals than the operator precedence</returns>
+        private bool CheckPrecedence(string token, string lastOperator, List<string> operators)
+        {
+            int operatorIndex = operators.FindIndex(x => x.Equals(lastOperator));
+            int tokenIndex = operators.FindIndex(x => x.Equals(token));
+            return tokenIndex >= operatorIndex;
+        }
+
         /// <summary>Tokenize the regular expression</summary>
         /// <param name="regex">The regular expression</param>
         /// <returns>A list with all the tokens</returns>
