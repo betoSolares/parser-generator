5ee59e05226c5dccd66c1660352f01814e279fc8
Roberto Solares <betosolaresgar@gmail.com>
2020-03-02 17:20:00 -0600

    Add the node for the tree

---
+++ scanner-generator/Regex/Node.cs
@@ -0,0 +1,30 @@
+﻿namespace Regex
+{
+    class Node
+    {
+        public string Value { get; private set; }
+        public Node LeftChild { get; set; }
+        public Node RightChild { get; set; }
+        public Node Parent { get; private set; }
+
+        /// <summary>Constructor</summary>
+        /// <param name="value">The new value</param>
+        public Node(string value)
+        {
+            Value = value;
+            LeftChild = null;
+            RightChild = null;
+        }
+
+        /// <summary>Constructor</summary>
+        /// <param name="value">The new value</param>
+        /// <param name="parent">The parent node</param>
+        public Node(string value, Node parent)
+        {
+            Value = value;
+            LeftChild = null;
+            RightChild = null;
+            Parent = parent;
+        }
+    }
+}
