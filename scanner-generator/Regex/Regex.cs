173dcd0ffe8601e75bf1ad8da8727b2aed1ff950
Roberto Solares <betosolaresgar@gmail.com>
2020-03-02 17:25:52 -0600

    Create the base class for the REGEX

---
+++ scanner-generator/Regex/Regex.cs
@@ -0,0 +1,18 @@
+﻿using Regex;
+
+namespace scanner_generator.Regex
+{
+    class Regex
+    {
+        public string Expression { get; private set; }
+        public Node Tree { get; private set; }
+
+        /// <summary>Constructor</summary>
+        /// <param name="regex">The regular expression to use</param>
+        public Regex(string regex)
+        {
+            Expression = regex;
+            CreateTree(regex);
+        }
+    }
+}
