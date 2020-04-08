fb951818c780015d29d388dce67c4bb49671dd45
Roberto Solares <betosolaresgar@gmail.com>
2020-04-08 16:21:04 -0600

    Implement the tree for the regular expression of the TOKENS

--- scanner-generator/UI/MachineView.cs
+++ scanner-generator/UI/MachineView.cs
@@ -1,17 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using RegularExpression;
+using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace scanner_generator.UI
 {
     public partial class MachineView : Form
     {
-        private readonly string regex = string.Empty;
+        private readonly Regex regex = null;
         
         /// <summary>Constructor</summary>
         /// <param name="tokens">The dictionary with the tokens</param>
         public MachineView(Dictionary<string, string> tokens)
         {
             InitializeComponent();
+            regex = new Regex(MakeExpression(tokens));
         }
 
         /// <summary>Create the regular expressions for the tokens</summary>
