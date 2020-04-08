a947a0eed6c32790cd9d5b6a378d6b0aa1592d7b
Roberto Solares <betosolaresgar@gmail.com>
2020-04-08 16:28:37 -0600

    Add a class to work with everything that is related with the creation of an regular expression from the tokens

---
+++ scanner-generator/Helpers/Expression.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+
+namespace Helpers
+{
+    public class Expression
+    {
+        /// <summary>Create the regular expressions for the tokens</summary>
+        /// <param name="tokens">The dictionary with the tokens</param>
+        /// <returns>The regular expression contructed</returns>
+        public string MakeExpression(Dictionary<string, string> tokens)
+        {
+            string regex = string.Empty;
+            foreach (KeyValuePair<string, string> token in tokens)
+            {
+                regex += "|(";
+                string[] elements = token.Value.Split(' ');
+                foreach (string part in elements)
+                {
+                    List<string> opertors = new List<string>
+                    {
+                        "(",
+                        ")",
+                        "*",
+                        "+",
+                        "?",
+                        "·",
+                        "|"
+                    };
+                    if (part.Length > 1)
+                    {
+                        string evaluating = part;
+                        int cont = 0;
+                        while (cont < part.Length)
+                        {
+                            if (evaluating.Length >= 3 && evaluating[0].ToString().Equals("'")
+                                && evaluating[2].ToString().Equals("'"))
+                            {
+                                regex += evaluating.Substring(0, 3) + "·";
+                                evaluating = evaluating.Remove(0, 3);
+                                cont += 3;
+                            }
+                            else if (opertors.Contains(evaluating[0].ToString()))
+                            {
+                                if (regex[regex.Length - 1].Equals('·') && !evaluating[0].Equals('('))
+                                {
+                                    regex = regex.Remove(regex.Length - 1, 1);
+                                }
+
+                                if (evaluating[0].Equals('*') || evaluating[0].Equals('+') || evaluating[0].Equals('?'))
+                                {
+                                    regex += evaluating.Substring(0, 1) + "·";
+                                }
+                                else
+                                {
+                                    regex += evaluating.Substring(0, 1);
+                                }
+
+                                evaluating = evaluating.Remove(0, 1);
+                                cont += 1;
+                            }
+                            else
+                            {
+                                regex += evaluating + "·";
+                                cont = evaluating.Length;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (opertors.Contains(part))
+                        {
+                            if (regex[regex.Length - 1].Equals('·') && !part.Equals("("))
+                            {
+                                regex = regex.Remove(regex.Length - 1, 1);
+                            }
+
+                            if (part.Equals('*') || part.Equals('+') || part.Equals('?'))
+                            {
+                                regex += part + "·";
+                            }
+                            else
+                            {
+                                regex += part;
+                            }
+                        }
+                    }
+                }
+
+                if (regex[regex.Length - 1].Equals('·'))
+                {
+                    regex = regex.Remove(regex.Length - 1, 1);
+                }
+                regex += ")";
+            }
+            return regex.Remove(0, 1);
+        }
+    }
+}
--- scanner-generator/UI/MachineView.cs
+++ scanner-generator/UI/MachineView.cs
@@ -1,4 +1,5 @@
-﻿using RegularExpression;
+﻿using Helpers;
+using RegularExpression;
 using System.Collections.Generic;
 using System.Windows.Forms;
 
@@ -6,104 +7,15 @@ namespace scanner_generator.UI
 {
     public partial class MachineView : Form
     {
+        private readonly Expression expression = new Expression();
         private readonly Regex regex = null;
-        
+
         /// <summary>Constructor</summary>
         /// <param name="tokens">The dictionary with the tokens</param>
         public MachineView(Dictionary<string, string> tokens)
         {
             InitializeComponent();
-            regex = new Regex(MakeExpression(tokens));
-        }
-
-        /// <summary>Create the regular expressions for the tokens</summary>
-        /// <param name="tokens">The dictionary with the tokens</param>
-        /// <returns>The regular expression contructed</returns>
-        private string MakeExpression(Dictionary<string, string> tokens)
-        {
-            string regex = string.Empty;
-            foreach (KeyValuePair<string, string> token in tokens)
-            {
-                regex += "|(";
-                string[] element = token.Value.Split(' ');
-                foreach (string part in element)
-                {
-                    List<string> opertors = new List<string>
-                    {
-                        "(",
-                        ")",
-                        "*",
-                        "+",
-                        "?",
-                        "·",
-                        "|"
-                    };
-                    if (part.Length > 1)
-                    {
-                        string evaluatedText = part;
-                        int cont = 0;
-                        while (cont < part.Length)
-                        {
-                            if (evaluatedText.Length >= 3 && evaluatedText[0].ToString().Equals("'") && evaluatedText[2].ToString().Equals("'"))
-                            {
-                                regex += evaluatedText.Substring(0, 3) + "·";
-                                evaluatedText = evaluatedText.Remove(0, 3);
-                                cont += 3;
-                            }
-                            else if (opertors.Contains(evaluatedText[0].ToString()))
-                            {
-                                if (regex[regex.Length - 1].Equals('·') && !evaluatedText[0].Equals('('))
-                                {
-                                    regex = regex.Remove(regex.Length - 1, 1);
-                                }
-
-                                if (evaluatedText[0].Equals('*') || evaluatedText[0].Equals('+') || evaluatedText[0].Equals('?'))
-                                {
-                                    regex += evaluatedText.Substring(0, 1) + "·";
-                                }
-                                else
-                                {
-                                    regex += evaluatedText.Substring(0, 1);
-                                }
-
-                                evaluatedText = evaluatedText.Remove(0, 1);
-                                cont += 1;
-                            }
-                            else
-                            {
-                                regex += evaluatedText + "·";
-                                cont = evaluatedText.Length;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        if (opertors.Contains(part))
-                        {
-                            if (regex[regex.Length - 1].Equals('·') && !part.Equals("("))
-                            {
-                                regex = regex.Remove(regex.Length - 1, 1);
-                            }
-
-                            if (part.Equals('*') || part.Equals('+') || part.Equals('?'))
-                            {
-                                regex += part + "·";
-                            }
-                            else
-                            {
-                                regex += part;
-                            }
-                        }
-                    }
-                }
-
-                if (regex[regex.Length - 1].Equals('·'))
-                {
-                    regex = regex.Remove(regex.Length - 1, 1);
-                }
-                regex += ")";
-            }
-            return regex.Remove(0, 1);
+            regex = new Regex(expression.MakeExpression(tokens));
         }
     }
 }
