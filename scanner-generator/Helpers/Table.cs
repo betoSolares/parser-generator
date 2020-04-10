0222a074b24b13a7c868f65a12cc73a4ab9bea36
Roberto Solares <betosolaresgar@gmail.com>
2020-04-10 09:33:15 -0600

    Add method to generate a string for a list

---
+++ scanner-generator/Helpers/Table.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Helpers
+{
+    public class Table
+    {
+        /// <summary>Create a string with the elements on the list</summary>
+        /// <param name="list">The list with the elements</param>
+        /// <returns>A string with each element separated by a comma</returns>
+        public string GetList(List<int> list)
+        {
+            string text = string.Empty;
+            foreach(int number in list)
+            {
+                text += number + ", ";
+            }
+            return text.Remove(text.Length - 2, 2);
+        }
+    }
+}
